ac5a1afe50614f6d604be2c916927a7068b27d43
Nora Zhou <nzhou@smith.edu>
2024-09-25 10:28:24 -0400

    submit

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,119 @@
-class Conversation {
+import java.util.Scanner;
+import java.util.Random;
 
+public class Conversation {
+
+  /* Integer to store the rounds of conversation */
+  public static Integer round;
+  /* Scanner to scan the input */
+  public static Scanner input = new Scanner(System.in);
+  /* Random to  get a random number */
+  public static Random random = new Random();
+  /* A list of strings to store the transcript */
+  public static String transcript = "";
+
+  /**
+   * Constructor
+   * @param number
+   */
+  public Conversation(Integer number) {
+    round = number;
+  }
+
+  /**
+   * Get the rounds of the conversation */ 
+  public void rounds() {
+    System.out.println("How many rounds? ");
+    // get the number of rounds from the user input
+    round = input.nextInt();
+    input.nextLine();
+  }
+
+  /**
+   * Mirror input from the user */  
+  public void mirror() {
+    // create a loop for rounds
+    while (round > 0) {
+      String userInput = input.nextLine();
+      // add user input to the transcript
+      transcript += userInput + "\n";
+
+      // create an empty string for storing user responses
+      String response = "";
+      // Set replacment to be false
+      boolean replacement = false;
+      // create a list for splitted words from the response
+      String[] words = userInput.split(" ");
+
+      // loop through each word in the list to check if it needs to be mirroed and replace
+      for (int i = 0; i < words.length; i++) {
+        if (words[i].equals("I")) {
+          words[i] = "you";
+          // turn on the replacement
+          replacement = true;
+        } else if (words[i].equals("me")) {
+          words[i] = "you";
+          replacement = true;
+        } else if (words[i].equals("am")) {
+          words[i] = "are";
+          replacement = true;
+        } else if (words[i].equals("you")) {
+          words[i] = "I";
+          replacement = true;
+        } else if (words[i].equals("my")) {
+          words[i] = "your";
+          replacement = true;
+        } else if (words[i].equals("your")) {
+          words[i] = "my";
+          replacement = true;
+        }
+      }
+
+      // check if replacment was made
+      if (replacement) {
+        // glue words together to response and add an question mark at the end
+        response = String.join(" ", words) + "?";
+      } else {
+        // create an array of fixed responses
+        String[] fixedResponse = {
+          "Okay.",
+          "Yeah",
+          "Mmm-hm."
+        };
+        // get a random index from the list
+        int index = random.nextInt(fixedResponse.length);
+        // get the selected response
+        response = fixedResponse[index];
+      }
+
+      // print the response
+      System.out.println(response);
+      // add to the transcript
+      transcript += response + "\n";
+
+      // Decrement round
+      round -= 1;
+    }
+  }
+  
   public static void main(String[] arguments) {
-    // You will start the conversation here.
+    Conversation myConversation = new Conversation(2);
+    myConversation.rounds();
+
+    // print a short greeting to start the conversation
+    System.out.println("Hi there!  What's on your mind?");
+    // add to the transcript
+    transcript += "Hi there!  What's on your mind?" + "\n";
+
+    // call the mirror function to get responses
+    myConversation.mirror();
+
+    // print the ending sentence
+    System.out.println("See ya!");
+    // add to the transcript
+    transcript += "See ya!" + "\n";
+
+    // print the transcript
+    System.out.println("\n" + "TRANSCRIPT:" + "\n" + transcript);
   }
 }
--- reflection.md
+++ reflection.md
@@ -1,7 +1,16 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+    Renee and Katrina
 - Which session(s) of TA / office hours did you attend?
-- What are your initial impressions of Java? 
-- Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+    Monday 7-9pm & Wednesday 8:30-10:30am
+- What are your initial impressions of Java?
+    They are many sytax rules to follow. For example, I need to define the class, objects and functions upfront. 
+    Logic and steps are important becuase they help break the pragram into manageable parts to code. 
+- Can you draw any conclusion about programming in general from the similarities or the differences between the two languages?
+    Procedural logic and basic structures, such as if/else statements, for loops, and main methods. However, there's more emphasis on methods in Java, reflecting its object-oriented nature. 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+    I understood the expectations of the chatbot, but sometimes there was a gap between what I wanted and the what was returned. 
+    My advice would be to start early and aim small. 
+    Think about what you want to implement and write down pseudocode to help with the logic. 
+    Then, fix the code step by step.  
